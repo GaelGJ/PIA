6ff94d79b16795a1fb4390d1c2d998cabb6f5d9a
Gael Garza Jimenez <123291165+GaelGJ@users.noreply.github.com>
2023-11-11 02:42:25 -0600

    Delete Mi Biografia/Mi Biografia directory

--- Mi Biografia/Mi Biografia/Javscript/Funciones.js
+++
@@ -1,7 +0,0 @@
-function siguiente(){
-    document.getElementById('imagen').src= "../Imagenes/Mi foto1.jpg";
-}
-
-function anterior(){
-    document.getElementById('imagen').src= "../Imagenes/Mi foto.jpg";
-}
