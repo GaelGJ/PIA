214f9c0d5612947412a640b2b099fba119b85388
GaelGJ <gaelgarzajimenez@gmail.com>
2023-11-11 02:29:22 -0600

    PIA

---
+++ Mi Biografia/Javscript/Funciones.js
@@ -0,0 +1,7 @@
+function siguiente(){
+    document.getElementById('imagen').src= "../Imagenes/Mi foto1.jpg";
+}
+
+function anterior(){
+    document.getElementById('imagen').src= "../Imagenes/Mi foto.jpg";
+}
